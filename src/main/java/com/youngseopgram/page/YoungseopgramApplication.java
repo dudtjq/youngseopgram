e8fb83248fe2a46403720cc9b47fc1cc58a7ce5e
DESKTOP-KM2AE57\user <dudtjq>
2023-03-16 18:41:52 +0900

    [이영섭] 1. sns 파일 최초 파일 추가

---
+++ src/main/java/com/youngseopgram/page/YoungseopgramApplication.java
@@ -0,0 +1,13 @@
+package com.youngseopgram.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungseopgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungseopgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngseopgram/page/YoungseopgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngseopgram.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungseopgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
